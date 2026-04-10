a4cad6d56573ec9fd6492325cbf146737ca2ae77
Shirayuki39 <lorddemecrius83@proton.me>
2026-04-11 06:50:45 +0800

    LH7n: Switch to libtranlog stub
    * libtranlog is useless, it's a telemetry library
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -471,7 +471,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek/libaedv \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/millennium
+    hardware/millennium \
+    hardware/millennium/libtranlog
 
 # Thermal
 PRODUCT_PACKAGES += \
