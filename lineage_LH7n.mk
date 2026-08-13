d6b97198e315a468cbf8b596dbb0277ea1be1862
Shirayuki39 <lorddemecrius83@proton.me>
2026-08-13 19:34:07 +0800

    LH7n: Use 241014V737 fingerprint
    * the latest one apparently can't get strong
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- lineage_LH7n.mk
+++ lineage_LH7n.mk
@@ -24,7 +24,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=LH7n \
-    BuildFingerprint=TECNO/LH7n-GL/TECNO-LH7n:14/UP1A.231005.007/250416V781:user/release-keys
+    BuildFingerprint=TECNO/LH7n-GL/TECNO-LH7n:14/UP1A.231005.007/241014V737:user/release-keys
 
 # Time
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
