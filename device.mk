8d669df99902b310f45c6a3ab82b00eb4e99d02b
SGCMarkus <markusornik@gmail.com>
2026-08-17 02:17:17 +0800

    LH7n: Set BOARD_[SHIPPING]_API_LEVEL to 31
    Change-Id: I2d8346b02e2b874e7ea76f4f0a36bf5764ea4a74
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -464,6 +464,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Shipping API level
+BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Soong namespaces
